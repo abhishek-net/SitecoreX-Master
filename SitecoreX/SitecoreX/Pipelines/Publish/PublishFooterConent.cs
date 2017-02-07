887b736d3d466fa91c00830cc50f1b16dcffeeb6
Abhishek Mishra <amishra42@sapient.com>
2017-02-07 21:13:30 +0000

    Adding missing file

---
+++ SitecoreX/SitecoreX/Pipelines/Publish/PublishFooterConent.cs
@@ -0,0 +1,25 @@
+﻿using Sitecore.Publishing.Pipelines.Publish;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SitecoreX.Pipelines.Publish
+{
+    class PublishFooterContent
+    {
+        public void OnPublishEnd(object sender, EventArgs args)
+        {
+            try
+            {
+                var footer = Sitecore.Data.Database.GetDatabase("web").GetItem("/Sitecore/Content/Components/Footer");
+                //var item = Sitecore.Context.Database.GetItem("{067F36FD-E2EF-4693-BB53-409278601BF4}");
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
